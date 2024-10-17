5764531b1fd213a548fd98260df925229a0a9917
cyberchai <chairachananharder@gmail.com>
2024-10-17 19:48:00 -0400

    init

---
+++ import random.py
@@ -0,0 +1,22 @@
+import random
+import datetime
+
+def begin():
+    print('=====================================================\n\t\t\tCSC 290 Chess Bot V.01 by Chaira, Juniper, Glory\t\t\t\n=====================================================')
+    print('Time', datetime.now())
+    print('Starting FEN positions? (hit ENTER for standard starting position): ')
+    
+    # wait this is actually game play I guess:
+    
+    # print('Bot (as white):')
+    # print('New FEN position:')
+    # print('Black:')
+    # print('New FEN position:')
+
+
+def chess():
+    # our chess function will go here
+    return 0
+
+def main():
+    chess()
