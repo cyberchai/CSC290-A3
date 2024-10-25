a4c31e27d5d92e76a3ea65c10b0380a99049e5f7
cyberchai <chairachananharder@gmail.com>
2024-10-25 12:54:06 -0400

    initial board made

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
 import random
 import datetime
 
-def begin():
-    print('=====================================================\n\t\t\tCSC 290 Chess Bot V.01 by Chaira, Juniper, Glory\t\t\t\n=====================================================')
-    print('Time', datetime.now())
-    print('Starting FEN positions? (hit ENTER for standard starting position): ')
+def init():
+    print('=====================================================\n\t\t\tCSC 290 Chess Bot V.01 by Chaira and Glory\t\t\t\n=====================================================')
+    print('Time:', datetime.datetime.now())
+    comp = input('Computer Player? (w=white/b=black): ')
+    starting_pos = input('Starting FEN position? (hit ENTER for standard starting position): ')
+    print('\n')
     
 
-def chess():
-    # our chess function will go here
-    return 0
+def update_board():
+    # This is our starting board. Black pieces are lower case, white pieces are upper case.
+    print('''    a   b   c   d   e   f   g   h
+8 | r | n | b | q | k | b | n | r | 8
+  ---------------------------------
+7 | p | p | p | p | p | p | p | p | 7
+  ---------------------------------
+6 |   |   |   |   |   |   |   |   | 6
+  ---------------------------------
+5 |   |   |   |   |   |   |   |   | 5
+  ---------------------------------
+4 |   |   |   |   |   |   |   |   | 4
+  ---------------------------------
+3 |   |   |   |   |   |   |   |   | 3
+  ---------------------------------
+2 | P | P | P | P | P | P | P | P | 2
+  ---------------------------------
+1 | R | N | B | Q | K | B | N | R | 1
+  a   b   c   d   e   f   g   h
+''')
 
 def main():
-    print('started')
-    begin()
-    chess()
+    init()
+
+    checkmate = False
+
+    while(not checkmate):
+        update_board()
+        checkmate = True
+
+if __name__ == '__main__':
+    main()
