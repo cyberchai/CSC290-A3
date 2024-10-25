cf5816dcf18f91cf71f0e548b6a157ffd7d0558f
cyberchai <chairachananharder@gmail.com>
2024-10-25 12:33:26 -0400

    file moved

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import random
+import datetime
 
+def begin():
+    print('=====================================================\n\t\t\tCSC 290 Chess Bot V.01 by Chaira, Juniper, Glory\t\t\t\n=====================================================')
+    print('Time', datetime.now())
+    print('Starting FEN positions? (hit ENTER for standard starting position): ')
+    
+
+def chess():
+    # our chess function will go here
+    return 0
 
 def main():
+    print('started')
+    begin()
     chess()
